db9c4c8b996593c1ea251d1526629765ece40e07
matteobonaccorso <32367029+matteobonaccorso@users.noreply.github.com>
2019-01-21 11:35:49 +0100

    modifica sql3

--- tabella.sql
+++ tabella.sql
@@ -1,1 +1,14 @@
-create table LogEntries(ID varchar(255) NOT NULL,PRIMARY KEY(ID),Timestamp varchar(255),Iface varchar(255),MTU varchar(255), Met varchar(255), RX_OK varchar(255),RX_ERR varchar(255),RX_DRP varchar(255), RX_OVR varchar(255), TX_OK varchar(255), TX_ERR varchar(255), TX_DRP varchar(255), TX_OVR varchar(255),Flg varchar(255));
+create table LogEntries(
+			ID varchar(255) NOT NULL,PRIMARY KEY(ID),
+			Timestamp varchar(255),Iface varchar(255),
+			MTU varchar(255), 
+			Met varchar(255), 
+			RX_OK varchar(255),
+			RX_ERR varchar(255),
+			RX_DRP varchar(255),
+			RX_OVR varchar(255), 
+			TX_OK varchar(255),
+			TX_ERR varchar(255), 
+			TX_DRP varchar(255),
+			TX_OVR varchar(255),
+			Flg varchar(255));
