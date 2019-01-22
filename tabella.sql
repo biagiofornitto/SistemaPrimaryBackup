e8bf141329382590429a1ba3875d7b99994a42bd
matteobonaccorso <32367029+matteobonaccorso@users.noreply.github.com>
2019-01-22 16:50:47 +0100

    Update tabella.sql

--- tabella.sql
+++ tabella.sql
@@ -1,16 +1,1 @@
-create LogEntries(
-			ID varchar(255) NOT NULL,
-	                PRIMARY KEY(ID),
-			tempo varchar(255),
-	                Iface varchar(255),
-			MTU varchar(255), 
-			Met varchar(255), 
-			RX_OK varchar(255),
-			RX_ERR varchar(255),
-			RX_DRP varchar(255),
-			RX_OVR varchar(255), 
-			TX_OK varchar(255),
-			TX_ERR varchar(255), 
-			TX_DRP varchar(255),
-			TX_OVR varchar(255),
-			Flg varchar(255));
+create LogEntries(ID varchar(255) NOT NULL, PRIMARY KEY(ID), tempo varchar(255), Iface varchar(255), MTU varchar(255), Met varchar(255), RX_OK varchar(255), RX_ERR varchar(255), RX_DRP varchar(255), RX_OVR varchar(255), TX_OK varchar(255), TX_ERR varchar(255), TX_DRP varchar(255), TX_OVR varchar(255), Flg varchar(255));
